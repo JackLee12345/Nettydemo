8dd6031403d10ff50fc37dc357aa49a0f3af4583
liyou <1014041610@qq.com>
2019-12-19 19:28:28 +0800

    case

--- src/main/java/com/nio/client/TimeClientHandle.java
+++ src/main/java/com/nio/client/TimeClientHandle.java
@@ -2,17 +2,122 @@ package com.nio.client;
 
 import com.sun.org.apache.bcel.internal.generic.Select;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 public class TimeClientHandle implements Runnable {
-    private String ip;
+    private String host;
 
     private int port;
 
-    public TimeClientHandle(String ip,int port){
-        this.ip = ip;
+    private Selector selector;
+
+    private SocketChannel socketChannel;
+
+    private volatile boolean flag;
+
+    public TimeClientHandle(String host, int port) {
+        this.host = host;
         this.port = port;
+        try {
+            selector = Selector.open();
+            socketChannel = SocketChannel.open();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void run() {
+        try {
+            doConnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        while (!flag) {
+            try {
+                selector.select();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectionKeys.iterator();
+                while (it.hasNext()) {
+                    SelectionKey key = it.next();
+                    it.remove();
+                    try {
+                        handInput(key);
+                    } catch (Exception e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+
+    private void handInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            if (key.isConnectable()) {
+                if (socketChannel.finishConnect()) {
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                    doWrite(socketChannel);
+                } else {
+                    System.exit(1);
+                }
+            }
+
+            if (key.isReadable()) {
+                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                int readBytes = socketChannel.read(byteBuffer);
+                if (readBytes > 0) {
+                    byteBuffer.flip();
+                    byte[] bytes = new byte[byteBuffer.remaining()];
+                    byteBuffer.get(bytes);
+                    String body = new String(bytes, "utf-8");
+                    System.out.println(body);
+                    this.flag = true;
+                } else if (readBytes < 0) {
+                    key.cancel();
+                    socketChannel.close();
+                } else{
+                    ;
+                }
+            }
+        }
+    }
+
+    private void doConnect() throws IOException {
+        if (socketChannel.connect(new InetSocketAddress(host, port))) {
+            socketChannel.register(selector, SelectionKey.OP_READ);
+            doWrite(socketChannel);
+        } else {
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        }
+
+    }
+
+    private void doWrite(SocketChannel socketChannel) throws IOException {
+        byte[] request = "Hello World".getBytes();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(request.length);
+        byteBuffer.put(request);
+        byteBuffer.flip();
+        socketChannel.write(byteBuffer);
+        if (!byteBuffer.hasRemaining()) {
+            System.out.println("Hello world 2 second time !");
+        }
     }
 }
--- src/main/java/com/spring/codes/SpringCodes.java
+++ src/main/java/com/spring/codes/SpringCodes.java
@@ -1,8 +1,13 @@
 package com.spring.codes;
 
+import com.Config;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SpringCodes {
 
     public static void main(String[] args) {
 
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        System.out.println(context);
     }
 }
